77cbd7ee09dfb1cf49101502775197609b4e48c7
ryusuke.niki@snowflake.com <ryusuke.niki@snowflake.com>
2023-02-19 08:56:47 +0000

    test2

---
+++ models/example/customer.sql
@@ -0,0 +1,16 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+{{ config(materialized="table") }}
+
+select c_custkey as customer_id, c_name as name, c_address as address
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+-- where id is not null
+from sample_data.tpch_sf1.customer
